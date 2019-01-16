a401aec04c77d5ce90fc1950d30db0c6a7e392d0
Nikola Dragan <nikoladragan@gmail.com>
2019-01-16 22:10:47 +0100

    HTML layout: chips and actions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<>
+				<div className="header"></div>
+				<div className="footer">
+					<div className="actions">
+						<button type="button" className="button">Hit</button>
+						<button type="button" className="button">Stand</button>
+						<button type="button" className="button">Split</button>
+						<button type="button" className="button">Double</button>
+						<button type="button" className="button">Insurance</button>
+						<button type="button" className="button">Surrender</button>
+					</div>
+					<div className="chips">
+						<div className="chip chip--peach">0.25€</div>
+						<div className="chip chip--white">1€</div>
+						<div className="chip chip--pink">2.5€</div>
+						<div className="chip chip--red">5€</div>
+						<div className="chip chip--blue">10€</div>
+						<div className="chip chip--yellow">20€</div>
+						<div className="chip chip--green">25€</div>
+						<div className="chip chip--black">100€</div>
+						<div className="chip chip--purple">500€</div>
+						<div className="chip chip--orange">1000€</div>
+						<div className="chip chip--grey chip--disabled">5000€</div>
+					</div>
+				</div>
+			</>
+		);
+	}
 }
 
 export default App;
